e0e4bf8490e5ede893a0f8ae85811ed855d792df
danielriveropaz <danielriveropaz@hotmail.com>
2019-05-12 22:04:31 +0200

    menu

--- JuegoHerencia/JuegoHerencia/Source.cpp
+++ JuegoHerencia/JuegoHerencia/Source.cpp
@@ -29,6 +29,16 @@ int main()
 	cout << endl;
 	cout << F.getCoste(0, 0);*/
 
+	char in;
+	cout << "Presione n para nuevo juego, c para cargar juego y e para salir: ";
+	cin >> in;
+	switch (in) {
+	case 'n':
+		break;
+	//case 'c':
+	case 'e':
+		return 0;
+	}
 	Fabrica F;
 	F.new_maquina(Fabrica::Autoclipper_M, 0, 0);
 	F.new_maquina(Fabrica::Marketing_M, 1, 0);
