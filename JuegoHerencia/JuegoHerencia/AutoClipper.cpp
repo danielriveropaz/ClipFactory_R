69c30bbd85e30b88bf8d86b43b65824f2bd0546a
danielriveropaz <danielriveropaz@hotmail.com>
2019-05-12 20:18:12 +0200

    Ajuste valores produccion

--- JuegoHerencia/JuegoHerencia/AutoClipper.cpp
+++ JuegoHerencia/JuegoHerencia/AutoClipper.cpp
@@ -9,8 +9,8 @@ using namespace std;
 AutoClipper::AutoClipper() :Pieza()
 {
 	float SetUPaporte[] = { 10,20,30,40,50,60,70,80,90,100 };
-	float SetUpconsumo[] = { 10,9,8,7,6,5,4,3,2,1 };
-	float SetUpcoste[]= { 10,20,40,80,90,100,110,30,100 };
+	float SetUpconsumo[] = { 10, 11, 13, 15, 20, 26, 30, 36, 41, 47 };
+	float SetUpcoste[]= { 10, 20, 40, 80, 90, 100, 110, 300, 100 };
 
 	for (int i = 0; i < TOP_LEVEL; i++)
 	{
